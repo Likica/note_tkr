53855b5af24d65dead11435921acfcf9eb057556
Coco Puff <fahrija_wyroski@live.com>
2021-05-27 17:21:50 -0500

    added file structure, added code to notesApi and route

---
+++ routes/apiRoutes/notesRoute.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const router = require('express').Router();
+const fs = require('fs');
+const { validateNote } = require('../../lib/notes');
+
+const notes = require('../../db/db.json');
+
+
+
+
+
+
+
+
+
+module.exports = router;
